7bf955dd5c526b0be6ee5bc2a7df2603f6eb327a
Jorge Reina <jorgereina.fl@gmail.com>
2026-01-13 19:48:09 +0100

    fix: resolve linting errors in optimize-images.js

--- optimize-images.js
+++ optimize-images.js
@@ -1,7 +1,7 @@
 import sharp from 'sharp';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { existsSync, mkdirSync, statSync } from 'fs';
+import { statSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
